108a3377c3f15e62ea737a04ad0d6430f547c3a2
weitkaemper <weitkaemper@german-uds.de>
2026-05-17 12:41:51 +0200

    Add models derived from Claus et al

---
+++ modelling_resources/causal_model_Claus_simplified.pl
@@ -0,0 +1,22 @@
+0.002 :: stroke. 
+0.078 :: weakness.
+0.063 :: sensory.
+0.047 :: visual. 
+0.005 :: facial_palsy.
+0.005 :: speech.
+
+fast :- weakness.
+fast :- facial_palsy.
+fast :- speech.
+e_fast :- fast.
+e_fast :- sensory.
+e_fast :- visual. 
+
+0.23 :: facial_palsy :- stroke. 
+0.49 :: speech :- stroke.
+0.48 :: weakness :- stroke.
+0.23 :: sensory :- stroke. 
+0.23 :: visual :- stroke.
+
+
+
---
+++ modelling_resources/detailed_causal_model_Claus_simplified.pl
@@ -0,0 +1,35 @@
+0.002 :: stroke. 
+0.078 :: weakness.
+0.063 :: sensory.
+0.047 :: visual. 
+0.005 :: facial_palsy.
+0.005 :: speech.
+
+fast :- weakness.
+fast :- facial_palsy.
+fast :- speech.
+e_fast :- fast.
+e_fast :- sensory.
+e_fast :- visual. 
+
+(409/900) :: tia; (254/900) :: minor; (237/900) :: major :- stroke.
+
+(36/409) :: facial_palsy :- tia. 
+(176/409) :: speech :- tia.
+(96/409) :: weakness :- tia.
+(69/409) :: weakness :- tia. 
+(100/409) :: visual :- tia.
+
+(47/254) :: facial_palsy :- minor. 
+(99/254) :: speech :- minor.
+(126/254) :: weakness :- minor.
+(89/254) :: weakness :- minor.
+(68/254) :: sensory :- minor. 
+(46/254) :: visual :- minor.
+
+(126/237) :: facial_palsy :- major. 
+(167/237) :: speech :- major.
+(211/237) :: weakness :- major.
+(183/237) :: sensory :- major. 
+(62/237) :: visual :- major.
+
